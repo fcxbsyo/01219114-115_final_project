8bc51a547647fd24c5df2806072d487f2e762061
Nisara PLOYSUTTIPOL <nisara.pl@ku.th>
2024-12-16 21:17:53 +0700

    Create SoundManager class to handle game sounds and music

---
+++ sound_manager.py
@@ -0,0 +1,14 @@
+import pygame
+
+
+class SoundManager:
+    def __init__(self):
+        pygame.mixer.init()
+        self.missile_sound = pygame.mixer.Sound("sound/missile.wav")
+        self.explosion_sound = pygame.mixer.Sound("sound/explosion.wav")
+        self.click_sound = pygame.mixer.Sound("sound/click.wav")
+        self.power_up_sound = pygame.mixer.Sound("sound/powerup.wav")  # Renamed to power_up_sound
+        pygame.mixer.music.load("sound/background_music.mp3")
+        pygame.mixer.music.play(-1)
+        pygame.mixer.music.set_volume(0.5)
+        self.is_paused = False
