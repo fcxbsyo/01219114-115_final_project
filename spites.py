7fa752a4992ba08c80507acdbe512f281e46c7cd
Nisara PLOYSUTTIPOL <nisara.pl@ku.th>
2024-12-16 20:06:31 +0700

    Added Asteroid class with random movement behavior and add get_heading_to function

--- spites.py
+++ spites.py
@@ -1,5 +1,6 @@
 import turtle
 import random
+import math
 
 
 class Sprite(turtle.Turtle):
@@ -44,3 +45,27 @@ class Ball(Sprite):
             "yellow": "deflect",
             "red": "explode"
         }.get(self.color()[0], None)
+
+
+def get_heading_to(t1, t2):  # Moved here to avoid circular import
+    x1 = t1.xcor()
+    y1 = t1.ycor()
+    x2 = t2.xcor()
+    y2 = t2.ycor()
+    heading = math.atan2(y1 - y2, x1 - x2)
+    heading = heading * 180.0 / 3.14159
+    return heading
+
+
+class Asteroid(Sprite):
+    def __init__(self, player):
+        super().__init__()
+        self.color("brown")
+        self.shape("asteroid")
+        self.speed = random.randint(2, 3) / 50
+        self.goto(0, 0)
+        heading = random.randint(0, 260)
+        distance = random.randint(300, 400)
+        self.setheading(heading)
+        self.fd(distance)
+        self.setheading(get_heading_to(player, self))
