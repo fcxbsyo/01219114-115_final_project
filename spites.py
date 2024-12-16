3a9644fa58ac55fb938928566036ab5d957a9738
Nisara PLOYSUTTIPOL <nisara.pl@ku.th>
2024-12-16 20:02:06 +0700

    Added Player class to represent the player in the game

--- spites.py
+++ spites.py
@@ -6,3 +6,11 @@ class Sprite(turtle.Turtle):
         turtle.Turtle.__init__(self)
         self.speed(0)
         self.penup()
+
+
+class Player(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("player")
+        self.score = 0
