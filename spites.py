7b6e7da43ced2be7f9fa41d8a75b91f8ba5decb5
Nisara PLOYSUTTIPOL <nisara.pl@ku.th>
2024-12-16 20:04:15 +0700

    Added Ball class with power-up behavior based on color

--- spites.py
+++ spites.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 
 class Sprite(turtle.Turtle):
@@ -25,3 +26,21 @@ class Missile(Sprite):
         self.speed = 1
         self.state = "ready"
         self.hideturtle()
+
+
+class Ball(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.shapesize(0.7)
+        self.speed = random.randint(1, 2)
+        self.state = "ready"
+        self.hideturtle()
+        # Assign power based on color
+        self.color(random.choice(["green", "blue", "yellow", "red"]))
+        self.power = {
+            "green": "increase_ball",
+            "blue": "freeze",
+            "yellow": "deflect",
+            "red": "explode"
+        }.get(self.color()[0], None)
