ab117aa4a63304f7fd5b059c0cb45dc4f735d28a
Nisara PLOYSUTTIPOL <nisara.pl@ku.th>
2024-12-16 22:02:27 +0700

    Update that user can exit even in main screen

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
 
     game.wn.onkey(game.start_game, "space")
     game.wn.onkey(game.show_instructions, "s")
+    game.wn.onkey(game.exit_game, "x")
 
     game.wn.listen()
     game.wn.mainloop()
